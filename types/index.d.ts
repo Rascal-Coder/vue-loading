787bf7c5ddb59ee0172348d53ee2351659b5d01c
qi yuhang <qiyuhang@thinkerx.com>
2024-01-08 19:33:56 +0800

    chore: 更新ts类型

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,51 +1,66 @@
-import {Plugin, Component} from 'vue'
+import { Plugin, Component } from "vue";
 
-export type LoaderType = 'spinner' | 'dots' | 'bars'
+export const animations = [
+  "RotateSquare",
+  "RotateSquare2",
+  "RotateSquare3",
+  "RotateSquare4",
+  "RotateSquare5",
+  "CubeShadow",
+  "Grid",
+  "Stretch",
+  "Circle",
+  "LetterCube",
+] as const;
 
+export type AnimationType = (typeof animations)[number];
 export interface Props {
-  active?: boolean,
-  canCancel?: boolean,
-  onCancel?: () => any,
-  isFullPage?: boolean,
-  transition?: string,
-  color?: string,
-  height?: number,
-  width?: number,
-  loader?: LoaderType | string,
-  backgroundColor?: string,
-  opacity?: number,
-  zIndex?: number,
-  enforceFocus?: boolean,
-  lockScroll?: boolean,
-  pauseOnHover?: boolean,
-  container?: Object | HTMLElement | Function,
+  active?: boolean;
+  canCancel?: boolean;
+  onCancel?: () => any;
+  isFullPage?: boolean;
+  transition?: string;
+  color?: string;
+  height?: number;
+  width?: number;
+  loader?: LoaderType | string;
+  backgroundColor?: string;
+  opacity?: number;
+  zIndex?: number;
+  enforceFocus?: boolean;
+  lockScroll?: boolean;
+  pauseOnHover?: boolean;
+  container?: Object | HTMLElement | Function;
 }
 
 export interface Slots {
-  default?: any
-  before?: any
-  after?: any
+  default?: any;
+  before?: any;
+  after?: any;
 }
 
 export interface ActiveLoader {
-  hide(): void
+  hide(): void;
 }
 
 export interface PluginApi {
-  show(props?: Props, slots?: Slots): ActiveLoader
+  show(props?: Props, slots?: Slots): ActiveLoader;
 }
 
-declare module '@vue/runtime-core' {
+declare module "@vue/runtime-core" {
   interface ComponentCustomProperties {
     readonly $loading: PluginApi;
   }
 }
 
-declare const LoadingPlugin: Plugin
-declare const Component: Component
+declare const LoadingPlugin: Plugin;
+declare const Component: Component;
 
-export declare function useLoading(globalProps?: Props, globalSlots?: Slots): PluginApi
+export declare function useLoading(
+  globalProps?: Props,
+  globalSlots?: Slots
+): PluginApi;
 
-export {LoadingPlugin};
+export { LoadingPlugin };
 
-export default Component
+export default Component;
